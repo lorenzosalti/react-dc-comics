6f54bc7a136a86b420b1e159c4004d8bb1eda6ce
lorenzosalti <lorenzosalti@gmail.com>
2025-04-07 15:22:18 +0200

    feat: Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ function Navbar() {
   return (
     <ul>
       <li><a href="#"><img src="./src/assets/images/buy-comics-digital-comics.png" alt="digital comics" /><p>digital comics</p></a></li>
-      <li><a href="#"><img src="./src/assets/images/buy-comics-merchandise.png" alt="digital comics" /><p>dc merchandise</p></a></li>
-      <li><a href="#"><img src="./src/assets/images/buy-comics-subscriptions.png" alt="digital comics" /><p>subscriptions</p></a></li>
-      <li><a href="#"><img src="./src/assets/images/buy-comics-shop-locator.png" alt="digital comics" /><p>comic shop locator</p></a></li>
-      <li><a href="#"><img src="./src/assets/images/buy-dc-power-visa.svg" alt="digital comics" /><p>dc power visa</p></a></li>
+      <li><a href="#"><img src="./src/assets/images/buy-comics-merchandise.png" alt="dc merchandise" /><p>dc merchandise</p></a></li>
+      <li><a href="#"><img src="./src/assets/images/buy-comics-subscriptions.png" alt="subscriptions" /><p>subscriptions</p></a></li>
+      <li><a href="#"><img src="./src/assets/images/buy-comics-shop-locator.png" alt="comic shop locator" /><p>comic shop locator</p></a></li>
+      <li><a href="#"><img src="./src/assets/images/buy-dc-power-visa.svg" alt="dc power visa" /><p>dc power visa</p></a></li>
     </ul>
 
   )
